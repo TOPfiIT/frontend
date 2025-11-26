f5dafb89cce815b19501779f15a7b9499ba0ddc7
InchanterX <inchanterx.work@gmail.com>
2025-11-26 15:00:20 +0300

    Added content and styles to privacy page

--- src/app/privacy/layout.tsx
+++ src/app/privacy/layout.tsx
@@ -1,14 +1,30 @@
+import styles from "./privacy.module.scss";
+import Link from "next/link";
+
 export default function PrivacyPageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <header>This is privacy page header</header>
-        <main>{children}</main>
-      </body>
-    </html>
+    <>
+      <header className={styles.header}>
+        <div className={styles.brand}>
+          <div className={styles.logoContainer}>
+            <img src="/Logo.svg" alt="logo" className={styles.logo} />
+          </div>
+          <div className={styles.nameContainer}>
+            <p className={styles.name}>Panopticum</p>
+          </div>
+        </div>
+        <div className={styles.topic}>
+          <p className={styles.authorization}>Регистрация</p>
+          <div className={styles.keyContainer}>
+            <img src="/security.svg" alt="securicty icon" className={styles.key} />
+          </div>
+        </div>
+      </header>
+      <main>{children}</main>
+    </>
   );
 }
