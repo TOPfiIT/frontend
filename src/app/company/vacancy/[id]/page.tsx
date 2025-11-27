4a4195b7d66ccde87e1ff2d59f751f1d3e81a358
InchanterX <inchanterx.work@gmail.com>
2025-11-27 13:04:20 +0300

    Added folding to cards

--- src/app/company/vacancy/[id]/page.tsx
+++ src/app/company/vacancy/[id]/page.tsx
@@ -1,10 +1,42 @@
 import styles from "../../company.module.scss";
+import { IntervieweeCard } from "./IntervieweeCard";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id: id } = await params;
+
+  const interviewees = [
+    {
+      id: 1,
+      name: "Алексей Иванов",
+      resumeLink: "https://drive.google.com/resume/alexey_ivanov.pdf",
+      timeSpent: "1:19:54",
+      taskTimes: "0:00:01, 1:09:53, 0:10:00",
+      tasksCompleted: 3,
+      suspiciousCopies: 3,
+      codeQuality: "4/5",
+      understanding: "3/5",
+      level: "middle",
+      recommendation: "казнить"
+    },
+    {
+      id: 2,
+      name: "Петр Сидоров",
+      resumeLink: "https://drive.google.com/resume/petr_sidorov.pdf",
+      timeSpent: "1:15:30",
+      taskTimes: "0:05:00, 1:00:30, 0:10:00",
+      tasksCompleted: 3,
+      suspiciousCopies: 1,
+      codeQuality: "5/5",
+      understanding: "4/5",
+      level: "senior",
+      recommendation: "нанять"
+    }
+  ];
+
   return (
     <div className={styles.vacancyPage}>
       <div className={styles.vacancyInfo}>
@@ -52,184 +84,13 @@ export default async function Page({
 
       <h2 className={styles.vacancyBlockTitle}>Результаты участников:</h2>
       <div className={styles.vacancyResults}>
-
-        <div className={styles.intervieweeCard}>
-          <div className={styles.intervieweeInfo}>
-            <p className={styles.intervieweeName}>Алексей Иванов</p>
-            <p className={styles.intervieweeResume}>Резюме: <a className={styles.intervieweeResumeLink} href="#">https://drive.google.com/resume/alexey_ivanov.pdf</a></p>
-          </div>
-          <div className={styles.intervieweeStatContainer}>
-            <div className={styles.intervieweeLogoContainer}>
-              <img className={styles.intervieweeLogo} src="/timer.svg" alt="timer icon" />
-            </div>
-            <p className={styles.intervieweeStat}><b>Потрачено:</b> 1:19:54</p>
-            <div className={styles.intervieweeSplit}></div>
-            <p className={styles.intervieweeStat}><b>На задания потрачено соответственно:</b> 0:00:01, 1:09:53, 0:10:00</p>
-            <div className={styles.intervieweeSplit}></div>
-            <p className={styles.intervieweeStat}><b>Заданий выполнено:</b> 3</p>
-            <div className={styles.intervieweeSplit}></div>
-            <p className={styles.intervieweeStat}><b>Количество подозрительных копирований:</b> 3</p>
-          </div>
-          <div className={styles.intervieweeDivider}></div>
-          <div className={styles.intervieweeResume}>
-            <h3 className={styles.intervieweeTitle}>Сводка от ИИ:</h3>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeNumbers}>
-            <div className={styles.intervieweeLogoContainer}>
-              <img className={styles.intervieweeLogo} src="/stat.svg" alt="statistic logo" />
-            </div>
-            <p className={styles.intervieweeStat}><b>Оценка чистоты кода:</b> 4/5</p>
-            <div className={styles.intervieweeSplit}></div>
-            <p className={styles.intervieweeStat}><b>Оценка понимания:</b> 3/5</p>
-          </div>
-          <div className={styles.intervieweeAssessment}>
-            <h3 className={styles.intervieweeTitle}>Оценка результата:</h3>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeLevel}>
-            <div className={styles.intervieweeTitleContainer}>
-              <div className={styles.intervieweeLogoContainer}>
-                <img className={styles.intervieweeLogo} src="/assess.svg" alt="assessment icon" />
-              </div>
-              <h3 className={styles.intervieweeTitle}>Технический уровень: middle</h3>
-            </div>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeDivider}></div>
-          <div className={styles.intervieweeStrengths}>
-            <div className={styles.intervieweeTitleContainer}>
-              <div className={styles.intervieweeLogoContainer}>
-                <img className={styles.intervieweeLogo} src="/strength.svg" alt="strength icon"></img>
-              </div>
-              <h3 className={styles.intervieweeTitle}>Сильные стороны</h3>
-            </div>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeWeaknesses}>
-            <div className={styles.intervieweeTitleContainer}>
-              <div className={styles.intervieweeLogoContainer}>
-                <img className={styles.intervieweeLogo} src="/weakness.svg" alt="weakness icon"></img>
-              </div>
-              <h3 className={styles.intervieweeTitle}>Слабые стороны</h3>
-            </div>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeCheats}>
-            <div className={styles.intervieweeTitleContainer}>
-              <div className={styles.intervieweeLogoContainer}>
-                <img className={styles.intervieweeLogo} src="/cheat.svg" alt="cheat icon"></img>
-              </div>
-              <h3 className={styles.intervieweeTitle}>Оценка читерства:</h3>
-            </div>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeVerdict}>
-            <div className={styles.intervieweeLogoContainer}>
-              <img className={styles.intervieweeLogo} src="/idea.svg" alt="idea icon" />
-            </div>
-            <div className={styles.intervieweeStatResult}>
-              <b>Итоговая оценка уровня: </b>
-              <p className={styles.activeMarking}>middle</p>
-            </div>
-            <div className={styles.intervieweeSplit}></div>
-            <div className={styles.intervieweeStatResult}>
-              <b>Рекомендация: </b>
-              <p className={styles.activeMarking}>казнить</p>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.intervieweeCard}>
-          <div className={styles.intervieweeInfo}>
-            <p className={styles.intervieweeName}>Алексей Иванов</p>
-            <p className={styles.intervieweeResume}>Резюме: <a className={styles.intervieweeResumeLink} href="#">https://drive.google.com/resume/alexey_ivanov.pdf</a></p>
-          </div>
-          <div className={styles.intervieweeStatContainer}>
-            <div className={styles.intervieweeLogoContainer}>
-              <img className={styles.intervieweeLogo} src="/timer.svg" alt="timer icon" />
-            </div>
-            <p className={styles.intervieweeStat}><b>Потрачено:</b> 1:19:54</p>
-            <div className={styles.intervieweeSplit}></div>
-            <p className={styles.intervieweeStat}><b>На задания потрачено соответственно:</b> 0:00:01, 1:09:53, 0:10:00</p>
-            <div className={styles.intervieweeSplit}></div>
-            <p className={styles.intervieweeStat}><b>Заданий выполнено:</b> 3</p>
-            <div className={styles.intervieweeSplit}></div>
-            <p className={styles.intervieweeStat}><b>Количество подозрительных копирований:</b> 3</p>
-          </div>
-          <div className={styles.intervieweeDivider}></div>
-          <div className={styles.intervieweeResume}>
-            <h3 className={styles.intervieweeTitle}>Сводка от ИИ:</h3>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeNumbers}>
-            <div className={styles.intervieweeLogoContainer}>
-              <img className={styles.intervieweeLogo} src="/stat.svg" alt="statistic logo" />
-            </div>
-            <p className={styles.intervieweeStat}><b>Оценка чистоты кода:</b> 4/5</p>
-            <div className={styles.intervieweeSplit}></div>
-            <p className={styles.intervieweeStat}><b>Оценка понимания:</b> 3/5</p>
-          </div>
-          <div className={styles.intervieweeAssessment}>
-            <h3 className={styles.intervieweeTitle}>Оценка результата:</h3>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeLevel}>
-            <div className={styles.intervieweeTitleContainer}>
-              <div className={styles.intervieweeLogoContainer}>
-                <img className={styles.intervieweeLogo} src="/assess.svg" alt="assessment icon" />
-              </div>
-              <h3 className={styles.intervieweeTitle}>Технический уровень: middle</h3>
-            </div>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeDivider}></div>
-          <div className={styles.intervieweeStrengths}>
-            <div className={styles.intervieweeTitleContainer}>
-              <div className={styles.intervieweeLogoContainer}>
-                <img className={styles.intervieweeLogo} src="/strength.svg" alt="strength icon"></img>
-              </div>
-              <h3 className={styles.intervieweeTitle}>Сильные стороны</h3>
-            </div>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeWeaknesses}>
-            <div className={styles.intervieweeTitleContainer}>
-              <div className={styles.intervieweeLogoContainer}>
-                <img className={styles.intervieweeLogo} src="/weakness.svg" alt="weakness icon"></img>
-              </div>
-              <h3 className={styles.intervieweeTitle}>Слабые стороны</h3>
-            </div>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeCheats}>
-            <div className={styles.intervieweeTitleContainer}>
-              <div className={styles.intervieweeLogoContainer}>
-                <img className={styles.intervieweeLogo} src="/cheat.svg" alt="cheat icon"></img>
-              </div>
-              <h3 className={styles.intervieweeTitle}>Оценка читерства:</h3>
-            </div>
-            <p className={styles.intervieweeText}>Lorem ipsum dolor sit amet consectetur. Iaculis netus ullamcorper nulla neque placerat malesuada pellentesque. Eu aliquam nullam lorem egestas. Proin etiam elementum condimentum convallis viverra. Ut volutpat sed nam non enim lectus. Mi ut eu lorem consequat viverra orci. Justo ullamcorper nibh erat molestie eget sollicitudin faucibus tincidunt urna. Eget eu mattis gravida elit ac lacus volutpat ut aliquam. Quisque rutrum non ullamcorper diam. Parturient ultricies quis augue ut ullamcorper.</p>
-          </div>
-          <div className={styles.intervieweeVerdict}>
-            <div className={styles.intervieweeLogoContainer}>
-              <img className={styles.intervieweeLogo} src="/idea.svg" alt="idea icon" />
-            </div>
-            <div className={styles.intervieweeStatResult}>
-              <b>Итоговая оценка уровня: </b>
-              <p className={styles.activeMarking}>middle</p>
-            </div>
-            <div className={styles.intervieweeSplit}></div>
-            <div className={styles.intervieweeStatResult}>
-              <b>Рекомендация: </b>
-              <p className={styles.activeMarking}>казнить</p>
-            </div>
-          </div>
-        </div>
-
+        {interviewees.map((interviewee) => (
+          <IntervieweeCard
+            key={interviewee.id}
+            interviewee={interviewee}
+          />
+        ))}
       </div>
-
-
     </div>
   );
 }
