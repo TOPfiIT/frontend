fde65e09615668337c942fe8f360405318babde0
Shashlyk <hi@ip.ru>
2025-11-27 13:18:28 +0300

    upd

--- src/models/vacancy.ts
+++ src/models/vacancy.ts
@@ -23,3 +23,15 @@ interface Vacancy {
   duration: number;
   created_at: string;
 }
+
+interface InterviewResults {
+  id: string;
+  vacancy_id: string;
+  name: string;
+  surname: string;
+  resume_link: string;
+  tasks: string[];
+  solutions: string[];
+  chat_history: string[];
+  metrics: string[];
+}
